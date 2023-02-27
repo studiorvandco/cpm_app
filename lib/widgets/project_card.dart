4c2976c6b76f878acf5968922776f85f64992a3b
Corentin CANIN <corentin.canin@gmail.com>
2023-02-27 14:31:35 +0100

    correct color favorite projetcs

--- lib/widgets/project_card.dart
+++ lib/widgets/project_card.dart
@@ -17,7 +17,11 @@ class ProjectCard extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => _ProjectCardState(
-      title: title, image: image, favorite: favorite, shotsTotal: shotsTotal, shotsCompleted: shotsCompleted);
+      title: title,
+      image: image,
+      favorite: favorite,
+      shotsTotal: shotsTotal,
+      shotsCompleted: shotsCompleted);
 }
 
 class _ProjectCardState extends State<ProjectCard> {
@@ -40,40 +44,47 @@ class _ProjectCardState extends State<ProjectCard> {
 
   @override
   Widget build(BuildContext context) {
-    Icon favIcon = favorite ? const Icon(Icons.star) : const Icon(Icons.star_border);
+    Icon favIcon = favorite
+        ? Icon(Icons.star, color: Theme.of(context).colorScheme.primary)
+        : const Icon(Icons.star_border);
     return Card(
         child: Padding(
       padding: const EdgeInsets.all(8),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-        Row(children: <Widget>[
-          SizedBox(
-            height: 80,
-            width: 80,
-            child: image,
-          ),
-          const SizedBox(width: 8),
-          Flexible(
-            fit: FlexFit.tight,
-            child: Text(
-              title,
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          IconButton(
-              onPressed: () {
-                toggleFavorite();
-                setState(() {
-                  favIcon = const Icon(Icons.star);
-                });
-              },
-              icon: favIcon),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.event))
-        ]),
-        const SizedBox(height: 8),
-        LinearProgressIndicator(value: shotsCompleted / shotsTotal),
-      ]),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(children: <Widget>[
+              SizedBox(
+                height: 80,
+                width: 80,
+                child: image,
+              ),
+              const SizedBox(width: 8),
+              Flexible(
+                fit: FlexFit.tight,
+                child: Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    toggleFavorite();
+                    setState(() {
+                      favIcon = const Icon(Icons.star);
+                    });
+                  },
+                  icon: favIcon),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.event))
+            ]),
+            const SizedBox(height: 8),
+            LinearProgressIndicator(value: shotsCompleted / shotsTotal),
+          ]),
     ));
   }
 }
