a81f69312cee04413f65519aa40c0ae693f1f174
TheRamsWay <34042770+TheRamsWay@users.noreply.github.com>
2023-02-27 09:29:38 +0100

    Added web member editor

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -4,6 +4,7 @@ import '../widgets/info_header.dart';
 
 import '../widgets/tabbed_info_sheet.dart';
 import '../widgets/web_member_card.dart';
+import '../widgets/web_member_editor.dart';
 
 class Test extends StatelessWidget {
   const Test({super.key});
@@ -11,37 +12,55 @@ class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        children: <Widget>[
-          InfoHeader(
-            title: 'Test',
-            description:
-                'This is a project, and this is a very long description that takes multiples lines, this project is a very good project that will be awesome',
-            dateRange: DateTimeRange(
-                start: DateTime(2023, 9, 19), end: DateTime(2023, 9, 24)),
-            leftLabel:
-                const IconLabel(text: 'Bastien', icon: Icons.movie_outlined),
-            rightLabel: const IconLabel(
-                text: 'Maxence', icon: Icons.description_outlined),
-            image: Image.asset('assets/en-sursis.png'),
-            progress: 0.3,
-            cornerButton:
-                IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
-          ),
-          WebMemberCard(
-              name: 'Member',
-              phone: '04 76 87 01 32',
-              picture: Image.asset('assets/logo-camera.png')),
-          TextButton(
-              onPressed: () {
-                showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return const TabbedInfoSheet();
-                    });
-              },
-              child: const Text('Bottom sheet')),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            InfoHeader(
+              title: 'Test',
+              description:
+                  'This is a project, and this is a very long description that takes multiples lines, this project is a very good project that will be awesome',
+              dateRange: DateTimeRange(
+                  start: DateTime(2023, 9, 19), end: DateTime(2023, 9, 24)),
+              leftLabel:
+                  const IconLabel(text: 'Bastien', icon: Icons.movie_outlined),
+              rightLabel: const IconLabel(
+                  text: 'Maxence', icon: Icons.description_outlined),
+              image: Image.asset('assets/en-sursis.png'),
+              progress: 0.3,
+              cornerButton: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.more_vert)),
+            ),
+            WebMemberCard(
+                name: 'Member',
+                phone: '04 76 87 01 32',
+                picture: Image.asset('assets/logo-camera.png')),
+            WebMemberEditor(onEdit: (){}, onSave: (){}, onCancel: (){}, name: 'Member', phone: '04 76 87 01 32', picture: Image.asset('assets/logo-camera.png')),
+            ElevatedButton(
+                onPressed: () {},
+                clipBehavior: Clip.antiAlias,
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                child: SizedBox(
+                    width: 170,
+                    height: 170,
+                    child: FittedBox(
+                        fit: BoxFit.cover,
+                        clipBehavior: Clip.hardEdge,
+                        child: Image.asset('assets/logo-camera.png')))),
+            TextButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const TabbedInfoSheet();
+                      });
+                },
+                child: const Text('Bottom sheet')),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/web_member_editor.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class WebMemberEditor extends StatelessWidget {
+  const WebMemberEditor(
+      {super.key,
+      required this.onEdit,
+      required this.onSave,
+      required this.onCancel,
+      this.picture,
+      this.name,
+      this.phone});
+
+  final void Function() onEdit;
+  final void Function() onSave;
+  final void Function() onCancel;
+  final Image? picture;
+  final String? name;
+  final String? phone;
+
+  Widget _getPicture() {
+    if (picture == null) {
+      return const Center(
+          child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Icon(Icons.camera_alt_outlined),
+      ));
+    } else {
+      return picture!;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 150),
+      child: SizedBox(
+        width: 200,
+        child: Column(
+          children: [
+            Text(
+              'Edit member',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 10,
+                child: SizedBox(
+                    width: 170,
+                    height: 170,
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: FittedBox(
+                              fit: BoxFit.cover,
+                              clipBehavior: Clip.hardEdge,
+                              child: _getPicture()),
+                        ),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: onEdit,
+                          ),
+                        )
+                      ],
+                    ))),
+            const SizedBox(
+              height: 32,
+            ),
+            TextFormField(
+              initialValue: name,
+              decoration: const InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                  isDense: true),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              initialValue: phone,
+              decoration: const InputDecoration(
+                  labelText: 'Phone number',
+                  border: OutlineInputBorder(),
+                  isDense: true),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: onEdit,
+                  style: OutlinedButton.styleFrom(
+                      foregroundColor: Theme.of(context).colorScheme.error,
+                      side: BorderSide(
+                          color: Theme.of(context).colorScheme.error)),
+                  child: const Text('Delete'),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                FilledButton(onPressed: onSave, child: const Text('Save')),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
