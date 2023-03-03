050489c51b26b59ff98f80b6489894d97e3c7753
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-03-03 17:03:25 +0100

    remove certificat bypass

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
@@ -12,17 +10,7 @@ import 'services/config.dart';
 import 'services/login.dart';
 import 'theme.dart';
 
-// TODO(mael): create certificate
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-  }
-}
-
 void main() async {
-  HttpOverrides.global = MyHttpOverrides();
   Intl.systemLocale = await findSystemLocale();
   WidgetsFlutterBinding.ensureInitialized();
   await Config.init();
