27763235e0a30f953003ada7e5e5596bc0cdce37
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-03-03 16:54:31 +0100

    use custom header

--- lib/pages/planning.dart
+++ lib/pages/planning.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import '../models/project.dart';
 import '../models/sequence.dart';
 
+enum View { month, week, day }
+
+final GlobalKey<MonthViewState> calendarKey = GlobalKey<MonthViewState>();
+
 class Planning extends StatefulWidget {
   const Planning({super.key, required this.project});
 
@@ -15,12 +19,11 @@ class Planning extends StatefulWidget {
 
 class _PlanningState extends State<Planning> with TickerProviderStateMixin {
   final List<CalendarEventData<String>> _events = <CalendarEventData<String>>[];
-  late TabController _tabController;
+  View view = View.month;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
 
     for (final Sequence sequence in widget.project.sequences) {
       _events.add(CalendarEventData<String>(
@@ -36,40 +39,83 @@ class _PlanningState extends State<Planning> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     CalendarControllerProvider.of(context).controller.addAll(_events);
 
-    return Expanded(
-      child: Column(
-        children: [
-          Wrap(
-            children: [
-              Material(
-                color: Theme.of(context).colorScheme.surface,
-                child: TabBar(
-                  controller: _tabController,
-                  tabs: const [
-                    Tab(icon: Icon(Icons.calendar_view_month), text: 'Month'),
-                    Tab(icon: Icon(Icons.calendar_view_week), text: 'Week'),
-                    Tab(icon: Icon(Icons.calendar_view_day), text: 'Day')
+    switch (view) {
+      case View.month:
+        return Expanded(child: _buildMonthView());
+      case View.week:
+        return Expanded(child: _buildWeekView());
+      case View.day:
+        return Expanded(child: _buildDayView());
+    }
+  }
+
+  HeaderStyle _buildHeader() {
+    return HeaderStyle(
+        decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
+        leftIcon: Row(
+          children: [
+            IconButton(
+                onPressed: () {
+                  calendarKey.currentState?.previousPage();
+                },
+                icon: const Icon(Icons.chevron_left)),
+            const Padding(padding: EdgeInsets.only(right: 8)),
+            IconButton(
+                onPressed: () {
+                  calendarKey.currentState?.nextPage();
+                },
+                icon: const Icon(Icons.chevron_right))
+          ],
+        ),
+        rightIcon: DropdownButton<View>(
+          focusColor: Colors.transparent,
+          value: view,
+          items: [
+            DropdownMenuItem<View>(
+                value: View.month,
+                child: Row(
+                  children: const <Widget>[
+                    Icon(Icons.calendar_view_month),
+                    Padding(padding: EdgeInsets.only(right: 8)),
+                    Text('Month'),
                   ],
-                ),
-              )
-            ],
-          ),
-          Expanded(
-              child: TabBarView(
-                  controller: _tabController, children: [_buildMonthView(), _buildWeekView(), _buildDayView()])),
-        ],
-      ),
-    );
+                )),
+            DropdownMenuItem<View>(
+                value: View.week,
+                child: Row(
+                  children: const <Widget>[
+                    Icon(Icons.calendar_view_week),
+                    Padding(padding: EdgeInsets.only(right: 8)),
+                    Text('Week'),
+                  ],
+                )),
+            DropdownMenuItem<View>(
+                value: View.day,
+                child: Row(
+                  children: const <Widget>[
+                    Icon(Icons.calendar_view_day),
+                    Padding(padding: EdgeInsets.only(right: 8)),
+                    Text('Day'),
+                  ],
+                ))
+          ],
+          onChanged: (View? newView) {
+            setState(() {
+              view = newView!;
+            });
+          },
+        ));
   }
 
   LayoutBuilder _buildMonthView() {
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
       return MonthView(
-        headerStyle: HeaderStyle(decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface)),
+        key: calendarKey,
+        headerStyle: _buildHeader(),
         minMonth: widget.project.beginDate,
         maxMonth: widget.project.endDate,
         initialMonth: DateTime.now(),
-        onPageChange: (DateTime date, int pageIndex) => print("$date, $pageIndex"),
+        onPageChange: (DateTime date, int pageIndex) => print('$date, $pageIndex'),
         onCellTap: (List<CalendarEventData<Object?>> events, DateTime date) {
           print(events);
         },
@@ -83,7 +129,7 @@ class _PlanningState extends State<Planning> with TickerProviderStateMixin {
   LayoutBuilder _buildWeekView() {
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
       return WeekView(
-        headerStyle: HeaderStyle(decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface)),
+        headerStyle: _buildHeader(),
         minDay: widget.project.beginDate,
         maxDay: widget.project.endDate,
         initialDay: DateTime.now(),
@@ -98,7 +144,7 @@ class _PlanningState extends State<Planning> with TickerProviderStateMixin {
   LayoutBuilder _buildDayView() {
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
       return DayView(
-        headerStyle: HeaderStyle(decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface)),
+        headerStyle: _buildHeader(),
         minDay: widget.project.beginDate,
         maxDay: widget.project.endDate,
         initialDay: DateTime.now(),
---
+++ lib/pages/sequences.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Sequences extends StatefulWidget {
+  const Sequences({super.key});
+
+  @override
+  State<Sequences> createState() => _SequencesState();
+}
+
+class _SequencesState extends State<Sequences> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
