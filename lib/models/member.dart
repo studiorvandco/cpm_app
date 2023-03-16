078b6de9bfa2cf08df59805df5a77e5ccd9f728b
Corentin CANIN <corentin.canin@gmail.com>
2023-03-16 10:56:02 +0100

    fix phone numbers

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Member {
-  Member({required this.id, required this.firstName, this.lastName, this.phone, this.image});
+  Member(
+      {required this.id,
+      required this.firstName,
+      this.lastName,
+      this.phone,
+      this.image});
 
   factory Member.fromJson(json) {
     return Member(
       id: json['Id'].toString(),
       firstName: json['FirstName'].toString(),
       lastName: json['LastName'].toString(),
-      phone: json['PhoneNumber'].toString(), // TODO(mael): rename to 'Phone' in backend
+      phone: json['Phone'].toString(),
     );
   }
 
@@ -16,7 +21,7 @@ class Member {
     return {
       'firstname': firstName,
       'lastname': lastName,
-      'phonenumber': phone,
+      'phone': phone,
     };
   }
 
