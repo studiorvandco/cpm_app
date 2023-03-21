c317ded702873e58f57895530e6e318cedf3c18f
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-03-21 23:45:19 +0100

    members and locations working

---
+++ lib/providers/locations.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'locations.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$locationsHash() => r'cb409751b8f131b0737ccaed3115d0472ee42baf';
+
+/// See also [Locations].
+@ProviderFor(Locations)
+final locationsProvider =
+    AutoDisposeAsyncNotifierProvider<Locations, List<Location>>.internal(
+  Locations.new,
+  name: r'locationsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$locationsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Locations = AutoDisposeAsyncNotifier<List<Location>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
