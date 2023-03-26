8886ec448f26dcc8a36892a44b25172486a05e47
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-03-26 20:40:24 +0200

    add delete methods

--- lib/widgets/info_headers/episode.dart
+++ lib/widgets/info_headers/episode.dart
@@ -22,68 +22,62 @@ class InfoHeaderEpisode extends ConsumerStatefulWidget {
 class _InfoHeaderEpisodeState extends ConsumerState<InfoHeaderEpisode> {
   @override
   Widget build(BuildContext context) {
-    return ref.watch(currentProjectProvider).when(data: (Project project) {
-      return ref.watch(currentEpisodeProvider).when(data: (Episode episode) {
-        return FilledButton(
-          style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.zero,
-              shape: const ContinuousRectangleBorder(),
-              backgroundColor: Theme.of(context).colorScheme.background),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Text(
-                            episode.title,
-                            style: Theme.of(context).textTheme.titleLarge,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
+    return ref.watch(currentEpisodeProvider).when(data: (Episode episode) {
+      return FilledButton(
+        style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.zero,
+            shape: const ContinuousRectangleBorder(),
+            backgroundColor: Theme.of(context).colorScheme.background),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          episode.title,
+                          style: Theme.of(context).textTheme.titleLarge,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        IconButton(
-                          onPressed: () => delete(project, episode),
-                          icon: const Icon(Icons.delete),
-                          color: Colors.red,
-                        )
-                      ],
-                    ),
-                    const Padding(padding: EdgeInsets.only(bottom: 8)),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(episode.description, maxLines: 2, overflow: TextOverflow.ellipsis)),
-                    Row(children: <Widget>[
-                      if (episode.director != null) ...<Widget>[
-                        const Padding(padding: EdgeInsets.only(bottom: 8)),
-                        const Flexible(child: Icon(Icons.movie_outlined))
-                      ],
-                      if (episode.writer != null) ...<Widget>[
-                        const Padding(padding: EdgeInsets.only(bottom: 8)),
-                        const Flexible(child: Icon(Icons.description_outlined))
-                      ]
-                    ])
-                  ],
-                ),
+                      ),
+                      IconButton(
+                        onPressed: () => delete(episode),
+                        icon: const Icon(Icons.delete),
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 8)),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(episode.description, maxLines: 2, overflow: TextOverflow.ellipsis)),
+                  Row(children: <Widget>[
+                    if (episode.director != null) ...<Widget>[
+                      const Padding(padding: EdgeInsets.only(bottom: 8)),
+                      const Flexible(child: Icon(Icons.movie_outlined))
+                    ],
+                    if (episode.writer != null) ...<Widget>[
+                      const Padding(padding: EdgeInsets.only(bottom: 8)),
+                      const Flexible(child: Icon(Icons.description_outlined))
+                    ]
+                  ])
+                ],
               ),
-            ],
-          ),
-          onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return const InfoSheetEpisode();
-                });
-          },
-        );
-      }, error: (Object error, StackTrace stackTrace) {
-        return requestPlaceholderError;
-      }, loading: () {
-        return requestPlaceholderLoading;
-      });
+            ),
+          ],
+        ),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return const InfoSheetEpisode();
+              });
+        },
+      );
     }, error: (Object error, StackTrace stackTrace) {
       return requestPlaceholderError;
     }, loading: () {
@@ -91,9 +85,14 @@ class _InfoHeaderEpisodeState extends ConsumerState<InfoHeaderEpisode> {
     });
   }
 
-  Future<void> delete(Project project, Episode episode) async {
+  Future<void> delete(Episode episode) async {
+    if (!ref.read(currentProjectProvider).hasValue) {
+      return;
+    }
+
     showConfirmationDialog(context, 'delete.lower'.tr()).then((bool? result) async {
       if (result ?? false) {
+        final Project project = ref.read(currentProjectProvider).value!;
         final Map<String, dynamic> result = await ref.read(episodesProvider.notifier).delete(project.id, episode.id);
         if (context.mounted) {
           final bool succeeded = result['succeeded'] as bool;
--- lib/widgets/info_headers/sequence.dart
+++ lib/widgets/info_headers/sequence.dart
@@ -2,11 +2,18 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../dialogs/confirm_dialog.dart';
+import '../../models/episode.dart';
+import '../../models/project.dart';
 import '../../models/sequence.dart';
+import '../../providers/episodes.dart';
+import '../../providers/navigation.dart';
+import '../../providers/projects.dart';
 import '../../providers/sequences.dart';
 import '../../utils/constants_globals.dart';
 import '../icon_label.dart';
 import '../info_sheets/sequence.dart';
+import '../snack_bars.dart';
 
 class InfoHeaderSequence extends ConsumerStatefulWidget {
   const InfoHeaderSequence({super.key});
@@ -30,14 +37,31 @@ class _InfoHeaderSequenceState extends ConsumerState<InfoHeaderSequence> {
               padding: const EdgeInsets.all(8),
               child: Column(
                 children: <Widget>[
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(sequence.title,
-                          style: Theme.of(context).textTheme.titleLarge, maxLines: 1, overflow: TextOverflow.ellipsis)),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          sequence.title,
+                          style: Theme.of(context).textTheme.titleLarge,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => delete(sequence),
+                        icon: const Icon(Icons.delete),
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
                   const Padding(padding: EdgeInsets.only(bottom: 8)),
                   Align(
                       alignment: Alignment.centerLeft,
-                      child: Text(sequence.description ?? '', maxLines: 2, overflow: TextOverflow.ellipsis)),
+                      child: Text(
+                        sequence.description ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      )),
                   const Padding(padding: EdgeInsets.only(bottom: 8)),
                   IconLabel(text: _getDateText(context, sequence), icon: Icons.event),
                   Row(children: <Widget>[
@@ -71,5 +95,29 @@ class _InfoHeaderSequenceState extends ConsumerState<InfoHeaderSequence> {
     final String firstText = DateFormat.yMd(context.locale.toString()).format(sequence.startDate);
     final String lastText = DateFormat.yMd(context.locale.toString()).format(sequence.endDate);
     return '$firstText - $lastText';
+  }
+
+  Future<void> delete(Sequence sequence) async {
+    if (!ref.read(currentProjectProvider).hasValue || !ref.read(currentEpisodeProvider).hasValue) {
+      return;
+    }
+
+    showConfirmationDialog(context, 'delete.lower'.tr()).then((bool? result) async {
+      if (result ?? false) {
+        final Project project = ref.read(currentProjectProvider).value!;
+        final Episode episode = ref.read(currentEpisodeProvider).value!;
+        final Map<String, dynamic> result =
+            await ref.read(sequencesProvider.notifier).delete(project.id, episode.id, sequence.id);
+        if (context.mounted) {
+          final bool succeeded = result['succeeded'] as bool;
+          final int code = result['code'] as int;
+          final String message =
+              succeeded ? 'snack_bars.sequence.deleted'.tr() : 'snack_bars.sequence.not_deleted'.tr();
+          ScaffoldMessenger.of(context)
+              .showSnackBar(CustomSnackBar().getModelSnackBar(context, succeeded, code, message: message));
+        }
+        ref.read(homePageNavigationProvider.notifier).set(HomePage.episodes);
+      }
+    });
   }
 }
