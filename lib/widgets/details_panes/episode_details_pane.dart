700c3216eb1776dc4965412da2087ac84d720602
Maël Chiotti <mael.chiotti@gmail.com>
2023-07-28 16:09:17 +0200

    [CPMF-106] Remove useless mixin

--- lib/widgets/details_panes/episode_details_pane.dart
+++ lib/widgets/details_panes/episode_details_pane.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../models/episode/episode.dart';
-import '../../models/project/project.dart';
 import '../../providers/episodes/episodes.dart';
-import '../../providers/projects/projects.dart';
 import '../../utils/constants_globals.dart';
 
 class EpisodeDetailsPane extends ConsumerStatefulWidget {
@@ -15,72 +13,56 @@ class EpisodeDetailsPane extends ConsumerStatefulWidget {
   ConsumerState<EpisodeDetailsPane> createState() => _DetailsPaneEpisodeState();
 }
 
-class _DetailsPaneEpisodeState extends ConsumerState<EpisodeDetailsPane>
-    with AutomaticKeepAliveClientMixin<EpisodeDetailsPane> {
+class _DetailsPaneEpisodeState extends ConsumerState<EpisodeDetailsPane> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
 
   @override
-  bool get wantKeepAlive => true;
-
-  @override
   Widget build(BuildContext context) {
-    super.build(context);
+    return ref.watch(currentEpisodeProvider).when(
+      data: (Episode episode) {
+        titleController.text = episode.getTitle;
+        descriptionController.text = episode.getDescription;
+        titleController.selection = TextSelection.collapsed(offset: titleController.text.length);
+        descriptionController.selection = TextSelection.collapsed(offset: descriptionController.text.length);
 
-    return ref.watch(currentProjectProvider).when(
-      data: (Project project) {
-        return ref.watch(currentEpisodeProvider).when(
-          data: (Episode episode) {
-            titleController.text = episode.getTitle;
-            descriptionController.text = episode.getDescription;
-            titleController.selection = TextSelection.collapsed(offset: titleController.text.length);
-            descriptionController.selection = TextSelection.collapsed(offset: descriptionController.text.length);
-
-            return Padding(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 8, top: 8, left: 8, right: 8),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Focus(
-                    onFocusChange: (bool hasFocus) {
-                      if (!hasFocus && titleController.text != project.title) {
-                        edit(episode);
-                      }
-                    },
-                    child: TextField(
-                      style: Theme.of(context).textTheme.titleMedium,
-                      decoration: InputDecoration.collapsed(hintText: 'attributes.title.upper'.tr()),
-                      controller: titleController,
-                      maxLength: 64,
-                    ),
-                  ),
-                  const Padding(padding: EdgeInsets.only(bottom: 16)),
-                  Focus(
-                    onFocusChange: (bool hasFocus) {
-                      if (!hasFocus && descriptionController.text != project.description) {
-                        edit(episode);
-                      }
-                    },
-                    child: TextField(
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      decoration: InputDecoration.collapsed(hintText: 'attributes.description.upper'.tr()),
-                      controller: descriptionController,
-                      keyboardType: TextInputType.multiline,
-                      minLines: 3,
-                      maxLines: null,
-                      maxLength: 280,
-                    ),
-                  ),
-                ],
+        return Padding(
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + 8, top: 8, left: 8, right: 8),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Focus(
+                onFocusChange: (bool hasFocus) {
+                  if (!hasFocus && titleController.text != episode.title) {
+                    edit(episode);
+                  }
+                },
+                child: TextField(
+                  style: Theme.of(context).textTheme.titleMedium,
+                  decoration: InputDecoration.collapsed(hintText: 'attributes.title.upper'.tr()),
+                  controller: titleController,
+                  maxLength: 64,
+                ),
               ),
-            );
-          },
-          error: (Object error, StackTrace stackTrace) {
-            return requestPlaceholderError;
-          },
-          loading: () {
-            return requestPlaceholderLoading;
-          },
+              const Padding(padding: EdgeInsets.only(bottom: 16)),
+              Focus(
+                onFocusChange: (bool hasFocus) {
+                  if (!hasFocus && descriptionController.text != episode.description) {
+                    edit(episode);
+                  }
+                },
+                child: TextField(
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  decoration: InputDecoration.collapsed(hintText: 'attributes.description.upper'.tr()),
+                  controller: descriptionController,
+                  keyboardType: TextInputType.multiline,
+                  minLines: 3,
+                  maxLines: null,
+                  maxLength: 280,
+                ),
+              ),
+            ],
+          ),
         );
       },
       error: (Object error, StackTrace stackTrace) {
--- lib/widgets/details_panes/shot_details_pane.dart
+++ lib/widgets/details_panes/shot_details_pane.dart
@@ -4,13 +4,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../models/episode/episode.dart';
-import '../../models/project/project.dart';
-import '../../models/sequence/sequence.dart';
 import '../../models/shot/shot_value.dart';
-import '../../providers/episodes/episodes.dart';
-import '../../providers/projects/projects.dart';
-import '../../providers/sequences/sequences.dart';
 import '../../utils/constants_globals.dart';
 
 class ShotDetailsPane extends ConsumerStatefulWidget {
@@ -20,101 +14,65 @@ class ShotDetailsPane extends ConsumerStatefulWidget {
   ConsumerState<ShotDetailsPane> createState() => _ShotDetailsPaneState();
 }
 
-class _ShotDetailsPaneState extends ConsumerState<ShotDetailsPane> with AutomaticKeepAliveClientMixin<ShotDetailsPane> {
+class _ShotDetailsPaneState extends ConsumerState<ShotDetailsPane> {
   TextEditingController descriptionController = TextEditingController();
   final List<String> values = ShotValue.labels();
   String? selectedValue;
 
   @override
-  bool get wantKeepAlive => true;
-
-  @override
   Widget build(BuildContext context) {
-    super.build(context);
+    return ref.watch(currentShotProvider).when(
+      data: (Shot shot) {
+        selectedValue = shot.value?.label;
+        descriptionController.text = shot.description ?? '';
+        descriptionController.selection = TextSelection.collapsed(offset: descriptionController.text.length);
 
-    return ref.watch(currentProjectProvider).when(
-      data: (Project project) {
-        return ref.watch(currentEpisodeProvider).when(
-          data: (Episode episode) {
-            return ref.watch(currentSequenceProvider).when(
-              data: (Sequence sequence) {
-                return ref.watch(currentShotProvider).when(
-                  data: (Shot shot) {
-                    selectedValue = shot.value?.label;
-                    descriptionController.text = shot.description ?? '';
-                    descriptionController.selection =
-                        TextSelection.collapsed(offset: descriptionController.text.length);
-
-                    return Padding(
-                      padding: EdgeInsets.only(
-                        bottom: MediaQuery.of(context).viewInsets.bottom + 8,
-                        top: 8,
-                        left: 8,
-                        right: 8,
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          DropdownButtonFormField<String>(
-                            isExpanded: true,
-                            hint: Text('shots.value.upper'.plural(1)),
-                            items: values.map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            value: selectedValue,
-                            onChanged: (String? value) {
-                              setState(() {
-                                selectedValue = value;
-                              });
-                              edit(shot);
-                            },
-                          ),
-                          const Padding(padding: EdgeInsets.only(bottom: 16)),
-                          Focus(
-                            onFocusChange: (bool hasFocus) {
-                              if (!hasFocus && descriptionController.text != project.description) {
-                                edit(shot);
-                              }
-                            },
-                            child: TextField(
-                              style: Theme.of(context).textTheme.bodyMedium,
-                              decoration: InputDecoration.collapsed(hintText: 'attributes.description.upper'.tr()),
-                              controller: descriptionController,
-                              keyboardType: TextInputType.multiline,
-                              minLines: 3,
-                              maxLines: null,
-                              maxLength: 280,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  error: (Object error, StackTrace stackTrace) {
-                    return requestPlaceholderError;
-                  },
-                  loading: () {
-                    return requestPlaceholderLoading;
-                  },
-                );
-              },
-              error: (Object error, StackTrace stackTrace) {
-                return requestPlaceholderError;
-              },
-              loading: () {
-                return requestPlaceholderLoading;
-              },
-            );
-          },
-          error: (Object error, StackTrace stackTrace) {
-            return requestPlaceholderError;
-          },
-          loading: () {
-            return requestPlaceholderLoading;
-          },
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom + 8,
+            top: 8,
+            left: 8,
+            right: 8,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              DropdownButtonFormField<String>(
+                isExpanded: true,
+                hint: Text('shots.value.upper'.plural(1)),
+                items: values.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: selectedValue,
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedValue = value;
+                  });
+                  edit(shot);
+                },
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 16)),
+              Focus(
+                onFocusChange: (bool hasFocus) {
+                  if (!hasFocus && descriptionController.text != shot.description) {
+                    edit(shot);
+                  }
+                },
+                child: TextField(
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  decoration: InputDecoration.collapsed(hintText: 'attributes.description.upper'.tr()),
+                  controller: descriptionController,
+                  keyboardType: TextInputType.multiline,
+                  minLines: 3,
+                  maxLines: null,
+                  maxLength: 280,
+                ),
+              ),
+            ],
+          ),
         );
       },
       error: (Object error, StackTrace stackTrace) {
