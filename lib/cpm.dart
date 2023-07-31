84862219d346bdcf277506f4c45e90c6c085512b
Maël Chiotti <mael.chiotti@gmail.com>
2023-07-31 12:07:16 +0200

    [CPMF-80] Use SF calendar

--- lib/cpm.dart
+++ lib/cpm.dart
@@ -1,4 +1,3 @@
-import 'package:calendar_view/calendar_view.dart';
 import 'package:cpm/pages/home.dart';
 import 'package:cpm/pages/login.dart';
 import 'package:cpm/providers/authentication/authentication.dart';
@@ -9,48 +8,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'models/event.dart';
-
 class CPM extends ConsumerWidget {
   const CPM({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return CalendarControllerProvider<Event>(
-      controller: EventController<Event>(),
-      child: ref.watch(themeProvider).when(
-        data: (ThemeMode theme) {
-          FlutterNativeSplash.remove();
+    return ref.watch(themeProvider).when(
+      data: (ThemeMode theme) {
+        FlutterNativeSplash.remove();
 
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: ref.watch(authenticationProvider).when(
-              data: (bool authenticated) {
-                return authenticated ? const Home() : const Login();
-              },
-              error: (Object error, StackTrace stackTrace) {
-                return const Login();
-              },
-              loading: () {
-                return const Login();
-              },
-            ),
-            title: 'Cinema Project Manager',
-            theme: Themes().light,
-            darkTheme: Themes().dark,
-            themeMode: theme,
-            localizationsDelegates: context.localizationDelegates,
-            supportedLocales: context.supportedLocales,
-            locale: context.locale,
-          );
-        },
-        error: (Object error, StackTrace stackTrace) {
-          return Container();
-        },
-        loading: () {
-          return Container();
-        },
-      ),
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: ref.watch(authenticationProvider).when(
+            data: (bool authenticated) {
+              return authenticated ? const Home() : const Login();
+            },
+            error: (Object error, StackTrace stackTrace) {
+              return const Login();
+            },
+            loading: () {
+              return const Login();
+            },
+          ),
+          title: 'Cinema Project Manager',
+          theme: Themes().light,
+          darkTheme: Themes().dark,
+          themeMode: theme,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+        );
+      },
+      error: (Object error, StackTrace stackTrace) {
+        return Container();
+      },
+      loading: () {
+        return Container();
+      },
     );
   }
 }
--- lib/pages/planning.dart
+++ lib/pages/planning.dart
@@ -1,12 +1,8 @@
-import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-import '../models/event.dart';
-import '../models/project/project.dart';
 import '../models/sequence/sequence.dart';
-import '../providers/projects/projects.dart';
 import '../providers/sequences/sequences.dart';
 import '../utils/constants_globals.dart';
 
@@ -17,38 +13,23 @@ class Planning extends ConsumerStatefulWidget {
   ConsumerState<Planning> createState() => _PlanningState();
 }
 
-class _PlanningState extends ConsumerState<Planning> with TickerProviderStateMixin {
+class _PlanningState extends ConsumerState<Planning> {
   @override
   Widget build(BuildContext context) {
-    return ref.watch(currentProjectProvider).when(
-      data: (Project project) {
-        return ref.watch(sequencesProvider).when(
-          data: (List<Sequence> sequences) {
-            CalendarControllerProvider.of<Event>(context).controller.addAll(<CalendarEventData<Event>>[
-              ...sequences.map((Sequence sequence) {
-                return CalendarEventData<Event>(
-                  event: Event(title: sequence.getTitle, description: sequence.description ?? ''),
-                  title: sequence.getTitle,
-                  date: sequence.getStartDate,
-                  startTime: sequence.startDate,
-                  endTime: sequence.endDate,
-                );
-              }),
-            ]);
-
-            return Expanded(
-              child: SfCalendar(
-                firstDayOfWeek: 1,
-                view: CalendarView.week,
-              ),
-            );
-          },
-          error: (Object error, StackTrace stackTrace) {
-            return requestPlaceholderError;
-          },
-          loading: () {
-            return requestPlaceholderLoading;
-          },
+    return ref.watch(sequencesProvider).when(
+      data: (List<Sequence> sequences) {
+        return Expanded(
+          child: SfCalendar(
+            firstDayOfWeek: 1,
+            showNavigationArrow: true,
+            showTodayButton: true,
+            view: CalendarView.schedule,
+            allowedViews: [
+              CalendarView.schedule,
+              CalendarView.day,
+              CalendarView.week,
+            ],
+          ),
         );
       },
       error: (Object error, StackTrace stackTrace) {
@@ -59,136 +40,4 @@ class _PlanningState extends ConsumerState<Planning> with TickerProviderStateMix
       },
     );
   }
-
-/* TODO(mael): rebuild planning page
-
-  HeaderStyle _buildHeader() {
-    return HeaderStyle(
-        decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
-        leftIcon: Row(
-          children: <Widget>[
-            IconButton(
-                onPressed: () {
-                  switch (view) {
-                    case CalendarView_.month:
-                      calendarMonthKey.currentState?.previousPage();
-                      break;
-                    case CalendarView_.week:
-                      calendarWeekKey.currentState?.previousPage();
-                      break;
-                    case CalendarView_.day:
-                      calendarDayKey.currentState?.previousPage();
-                      break;
-                  }
-                },
-                icon: const Icon(Icons.chevron_left)),
-            const Padding(padding: EdgeInsets.only(right: 8)),
-            IconButton(
-                onPressed: () {
-                  switch (view) {
-                    case CalendarView_.month:
-                      calendarMonthKey.currentState?.nextPage();
-                      break;
-                    case CalendarView_.week:
-                      calendarWeekKey.currentState?.nextPage();
-                      break;
-                    case CalendarView_.day:
-                      calendarDayKey.currentState?.nextPage();
-                      break;
-                  }
-                },
-                icon: const Icon(Icons.chevron_right))
-          ],
-        ),
-        rightIcon: DropdownButton<CalendarView_>(
-          focusColor: Colors.transparent,
-          value: view,
-          items: <DropdownMenuItem<CalendarView_>>[
-            DropdownMenuItem<CalendarView_>(
-                value: CalendarView_.month,
-                child: Row(
-                  children: <Widget>[
-                    const Icon(Icons.calendar_view_month),
-                    const Padding(padding: EdgeInsets.only(right: 8)),
-                    Text('planning.date.month.upper'.tr()),
-                  ],
-                )),
-            DropdownMenuItem<CalendarView_>(
-                value: CalendarView_.week,
-                child: Row(
-                  children: <Widget>[
-                    const Icon(Icons.calendar_view_week),
-                    const Padding(padding: EdgeInsets.only(right: 8)),
-                    Text('planning.date.week.upper'.tr()),
-                  ],
-                )),
-            DropdownMenuItem<CalendarView_>(
-                value: CalendarView_.day,
-                child: Row(
-                  children: <Widget>[
-                    const Icon(Icons.calendar_view_day),
-                    const Padding(padding: EdgeInsets.only(right: 8)),
-                    Text('planning.date.day.upper'.tr()),
-                  ],
-                ))
-          ],
-          onChanged: (CalendarView_? newView) {
-            setState(() {
-              view = newView!;
-            });
-          },
-        ));
-  }
-
-  LayoutBuilder _buildMonthView(Project project) {
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-      return MonthView<Event>(
-        key: calendarMonthKey,
-        headerStyle: _buildHeader(),
-        minMonth: project.startDate,
-        maxMonth: project.endDate,
-        initialMonth: DateTime.now(),
-        onPageChange: (DateTime date, int pageIndex) => print('$date, $pageIndex'),
-        onCellTap: (List<CalendarEventData<Object?>> events, DateTime date) {
-          print(events);
-        },
-        onEventTap: (CalendarEventData<Object?> event, DateTime date) => print(event),
-        onDateLongPress: (DateTime date) => print(date),
-        width: constraints.maxWidth,
-      );
-    });
-  }
-
-  LayoutBuilder _buildWeekView(Project project) {
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-      return WeekView<Event>(
-        key: calendarWeekKey,
-        headerStyle: _buildHeader(),
-        minDay: project.startDate,
-        maxDay: project.endDate,
-        initialDay: DateTime.now(),
-        eventArranger: const SideEventArranger<Event>(),
-        onEventTap: (List<CalendarEventData<Object?>> events, DateTime date) => print(events),
-        onDateLongPress: (DateTime date) => print(date),
-        width: constraints.maxWidth,
-      );
-    });
-  }
-
-  LayoutBuilder _buildDayView(Project project) {
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-      return DayView<Event>(
-        key: calendarDayKey,
-        headerStyle: _buildHeader(),
-        minDay: project.startDate,
-        maxDay: project.endDate,
-        initialDay: DateTime.now(),
-        eventArranger: const SideEventArranger<Event>(),
-        onEventTap: (List<CalendarEventData<Object?>> events, DateTime date) => print(events),
-        onDateLongPress: (DateTime date) => print(date),
-        width: constraints.maxWidth,
-      );
-    });
-  }
-  */
 }
