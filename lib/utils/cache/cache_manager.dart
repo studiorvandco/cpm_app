7fe914a4965cf193b27123af7f9a3a4d418b8371
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-11-21 12:38:07 +0100

    [148] Fix cache path

--- lib/utils/cache/cache_manager.dart
+++ lib/utils/cache/cache_manager.dart
@@ -1,5 +1,6 @@
 import 'package:cpm/models/base_model.dart';
 import 'package:cpm/utils/cache/cache_key.dart';
+import 'package:cpm/utils/platform_manager.dart';
 import 'package:json_cache/json_cache.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:path_provider/path_provider.dart';
@@ -17,7 +18,8 @@ class CacheManager {
   late final JsonCache _cache;
 
   Future<void> init() async {
-    _storage = LocalStorage('cache', (await getApplicationCacheDirectory()).path);
+    final cachePath = PlatformManager().isWeb ? null : (await getApplicationCacheDirectory()).path;
+    _storage = LocalStorage('cache', cachePath);
     _cache = JsonCacheMem(JsonCacheLocalStorage(_storage));
   }
 
