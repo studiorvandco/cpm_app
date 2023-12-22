b45c41a6238d49d45b5fbb9dd15552bd897e77ba
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-12-22 14:24:51 +0100

    [156] changement de librairie

--- lib/utils/lexo_ranker.dart
+++ lib/utils/lexo_ranker.dart
@@ -1,33 +1,17 @@
-import 'package:lexo_rank_generator/lexo_rank_generator.dart';
+import 'package:lexo_rank/lexo_rank.dart';
 
 class LexoRanker {
-  static final LexoRanker _singleton = LexoRanker._internal();
-
-  factory LexoRanker() {
-    return _singleton;
-  }
-
-  LexoRanker._internal();
-
-  // TODO implement a supabase-side rebalance when necessary
-  // This default rank currently allows to move items at the top 26*1000 = 26000 times without necessitating a rebalance
-  final defaultRank = List.generate(1000, (_) => 'a').join();
-
-  late final LexoRank _lexoRank;
-
-  Future<void> init() async {
-    _lexoRank = const LexoRank();
-  }
+  String get defaultRank => LexoRank.middle().value;
 
   String newRank({String? previous, String? next}) {
     if (previous == null && next == null) {
       return defaultRank;
     } else if (next == null) {
-      return _lexoRank.nextLexo(previous!);
+      return LexoRank.parse(previous!).genNext().value;
     } else if (previous == null) {
-      return _lexoRank.prevLexo(next);
+      return LexoRank.parse(next).genPrev().value;
     } else {
-      return _lexoRank.getRankBetween(firstRank: previous, secondRank: next);
+      return LexoRank.parse(previous).between(LexoRank.parse(next)).value;
     }
   }
 }
